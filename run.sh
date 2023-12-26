25b6f34b6239ba72c7e255ffe130f8ecdcb0a35a
Vladislav <vladislav_semykin01@mail.ru>
2023-12-26 17:30:10 +0300

    Modified: Script rewritten to start test files from 'tests/' folder to check results of intermediate results

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-g++ src/*.cpp -std=c++20 -Wall -Wpedantic -Wextra -laabb -DLOG -o main
-./main
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <test file to compile>"
+    exit 1
+fi
 
+g++ $1 $(ls src/*.cpp | grep -v 'main.cpp') $(root-config --cflags --glibs) -std=c++20 -laabb -Wall -Wpedantic -Wextra
+./a.out
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32;1mFile $1 successfully compiled and ran\e[0m"
+else
+    echo -e "\e[31;1mError: Compilation or execution failed\e[0m"
+fi
