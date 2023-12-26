c9c4618b0eac40a303df57fd293f7ef85a5a9c74
Vladislav <vladislav_semykin01@mail.ru>
2023-12-26 17:31:25 +0300

    Generating particles in bounded volume with updating positions in different models using GMSH

--- visualize.py
+++ visualize.py
@@ -1,53 +1,7 @@
 import gmsh
 import sys
-
-
-def create_bounding_box(size: float, mesh_factor: float = 10):
-    mesh_size = size / mesh_factor
-
-    p1 = gmsh.model.geo.addPoint(0, 0, 0, mesh_size)
-    p2 = gmsh.model.geo.addPoint(size, 0, 0, mesh_size)
-    p3 = gmsh.model.geo.addPoint(0, size, 0, mesh_size)
-    p4 = gmsh.model.geo.addPoint(size, size, 0, mesh_size)
-    p5 = gmsh.model.geo.addPoint(0, 0, size, mesh_size)
-    p6 = gmsh.model.geo.addPoint(size, 0, size, mesh_size)
-    p7 = gmsh.model.geo.addPoint(0, size, size, mesh_size)
-    p8 = gmsh.model.geo.addPoint(size, size, size, mesh_size)
-
-    l1 = gmsh.model.geo.addLine(p1, p2)
-    l2 = gmsh.model.geo.addLine(p1, p3)
-    l3 = gmsh.model.geo.addLine(p1, p5)
-    l4 = gmsh.model.geo.addLine(p2, p4)
-    l5 = gmsh.model.geo.addLine(p2, p6)
-    l6 = gmsh.model.geo.addLine(p3, p4)
-    l7 = gmsh.model.geo.addLine(p3, p7)
-    l8 = gmsh.model.geo.addLine(p4, p8)
-    l9 = gmsh.model.geo.addLine(p5, p6)
-    l10 = gmsh.model.geo.addLine(p5, p7)
-    l11 = gmsh.model.geo.addLine(p7, p8)
-    l12 = gmsh.model.geo.addLine(p6, p8)
-
-    cl1 = gmsh.model.geo.addCurveLoop([l1, l5, -l9, -l3])
-    cl2 = gmsh.model.geo.addCurveLoop([l2, l7, -l10, -l3])
-    cl3 = gmsh.model.geo.addCurveLoop([l6, l8, -l11, -l7])
-    cl4 = gmsh.model.geo.addCurveLoop([l12, l5, -l4, -l8])
-    cl5 = gmsh.model.geo.addCurveLoop([l1, l4, -l6, -l2])
-    cl6 = gmsh.model.geo.addCurveLoop([l9, l12, -l11, -l10])
-
-    surface1 = gmsh.model.geo.addPlaneSurface([cl1])
-    surface2 = gmsh.model.geo.addPlaneSurface([cl2])
-    surface3 = gmsh.model.geo.addPlaneSurface([cl3])
-    surface4 = gmsh.model.geo.addPlaneSurface([cl4])
-    surface5 = gmsh.model.geo.addPlaneSurface([cl5])
-    surface6 = gmsh.model.geo.addPlaneSurface([cl6])
-
-    # Generating tetrahedal volume
-    # volume = gmsh.model.geo.addSurfaceLoop(
-    #     [surface1, surface2, surface3, surface4, surface5, surface6]
-    # )
-    # gmsh.model.geo.addVolume([volume])
-
-    return [surface1, surface2, surface3, surface4, surface5, surface6]
+import random
+import os
 
 
 def read_particle_data(filename: str) -> list:
@@ -60,30 +14,71 @@ def read_particle_data(filename: str) -> list:
         return particles
 
 
-def spawn_particles():
-    particles = read_particle_data("particles.txt")
-    spheres = []
-    for particle in particles:
-        x, y, z, radius = particle
-        spheres.append(gmsh.model.occ.addSphere(x, y, z, radius))
-    return spheres
+def create_bounding_box(
+    x: float = 0,
+    y: float = 0,
+    z: float = 0,
+    dx: float = 100,
+    dy: float = 100,
+    dz: float = 100,
+):
+    gmsh.model.occ.addBox(x, y, z, dx, dy, dz)
+
+
+def spawn_particles(particles):
+    for x, y, z, radius in particles:
+        gmsh.model.occ.addSphere(x, y, z, radius)
+
+
+def update_positions(particles, time_step):
+    for i in range(len(particles)):
+        dx = random.uniform(-1, 1) * time_step
+        dy = random.uniform(-1, 1) * time_step
+        dz = random.uniform(-1, 1) * time_step
+
+        particles[i] = (
+            particles[i][0] + dx,
+            particles[i][1] + dy,
+            particles[i][2] + dz,
+            particles[i][3],  # Radius unchanged
+        )
+
+
+def write_in_pos_file(particles: list, filename: str, time_point):
+    with open(filename, "a") as file:
+        file.write(f'View "Particles_{time_point}"{{')
+        for particle in particles:
+            x, y, z, _ = particle
+            file.write(f"\nVP({x},{y},{z}){{0,0,0}};")
+        file.write("};\n")
+
+
+def animate(particles: list, time_step: float):
+    gmsh.model.add("Particles_in_Bounded_Cubic_Volume_" + str(time_step))
+
+    create_bounding_box()
+    update_positions(particles, time_step)
+    spawn_particles(particles)
+
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(2)  # 2D mesh (only surfaces)
+
+    return particles
 
 
 def __main__():
-    gmsh.initialize()
-    gmsh.model.add("particles")
+    gmsh.initialize(sys.argv)
 
-    # Create a bounding box of size 100x100x100
-    bounding_box = create_bounding_box(100)
-    particles = spawn_particles()
+    time_interval = 10
+    particles = read_particle_data("particles.txt")
+    path = os.path.dirname(os.path.abspath(__file__))
+    for time_step in range(time_interval):
+        animate(particles, time_step)
+        write_in_pos_file(particles, "view.pos", time_step)
 
-    gmsh.model.geo.synchronize()
-    gmsh.model.mesh.generate(3)
-    gmsh.write("particles.msh")
-
+    gmsh.merge(os.path.join(path, "view.pos"))
     if "-nopopup" not in sys.argv:
         gmsh.fltk.run()
-
     gmsh.finalize()
 
 
