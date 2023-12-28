e551e27098f95ac9a8e4f5c2444272e29f3636b5
Vladislav <vladislav_semykin01@mail.ru>
2023-12-28 09:06:24 +0300

    Modified: Added 'constinit' to static physical constants to ensure that these vars will be initialized at compile time

--- include/Settings.hpp
+++ include/Settings.hpp
@@ -66,23 +66,25 @@ namespace settings
      * @return String composed from all arguments
      */
     template <Printable... Args>
-    constexpr std::string gen_str(Args &&...args)
+    constexpr std::string stringify(Args &&...args)
     {
         std::ostringstream oss;
         (oss << ... << args);
         return oss.str();
     }
-}
 
-/* --- Constants --- */
-static constexpr double P{1};                                      // [Pa]
-static constexpr double R{8.314};                                  // [J/k*mol]
-static constexpr double V{1};                                      // [vol]
-static constexpr double T{300};                                    // [k]
-static constexpr double N_av{6.22e23};                             // Avogadro number
-static constexpr double KE_eV{1};                                  // [eV]
-static constexpr double KE_J{1.602176565e-19};                     // [J]
-static constexpr double dt{1e-6};                                  // [s] - time step
-static constexpr double n_concentration{(P * V / (T * R)) * N_av}; // [1/m^3]
+    namespace phtsical_constants
+    {
+        static constinit const double P{1};                                      // [Pa]
+        static constinit const double R{8.314};                                  // [J/k*mol]
+        static constinit const double V{1};                                      // [vol]
+        static constinit const double T{300};                                    // [k]
+        static constinit const double N_av{6.22e23};                             // Avogadro number
+        static constinit const double KE_eV{1};                                  // [eV]
+        static constinit const double KE_J{1.602176565e-19};                     // [J]
+        static constinit const double dt{1e-6};                                  // [s] - time step
+        static constinit const double n_concentration{(P * V / (T * R)) * N_av}; // [1/m^3]
+    }
+}
 
 #endif // !SETTINGS_HPP
