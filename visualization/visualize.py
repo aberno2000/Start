9e113c25a39fab411fe15465d86a1409980da7ad
Vladislav <vladislav_semykin01@mail.ru>
2024-01-08 17:31:13 +0300

    Modified: Files moved to new directory that will be contain python code to visualize different volumes in the future

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -1,97 +1,8 @@
-from inst_deps import check_and_install_packages
-
-# Installing dependencies
-check_and_install_packages(["gmsh", "numpy"])
-import gmsh
 import numpy
+from create_entities import *
 from sys import argv
-
-
-def read_file_content(filename: str) -> list:
-    """
-    Reads data from a file.
-
-    Args:
-    - filename (str): Name of the file containing any data.
-
-    Returns:
-    - list: List containing lines of content.
-    """
-    try:
-        with open(filename, "r") as file:
-            content = []
-            for line in file:
-                content.append(line)
-            return content
-    except FileNotFoundError:
-        return f"File '{filename}' not found"
-
-
-def read_particle_data(filename: str) -> [()]:
-    """
-    Gets particle data from a file.
-
-    Args:
-    - filename (str): Name of the file containing particle data.
-
-    Returns:
-    - list: List containing particle data as tuples.
-    """
-    particles = []
-    for line in read_file_content(filename):
-        data = line.split()
-        x, y, z, Vx, Vy, Vz, radius = map(float, data)
-        particles.append((x, y, z, Vx, Vy, Vz, radius))
-    return particles
-
-
-def create_particles(particles: [()]) -> [()]:
-    """
-    Create particles based on input data.
-
-    Args:
-    - particles (list of tuples): List containing particle data as tuples.
-
-    Returns:
-    - list of tuples: List of tuples representing particle positions and properties.
-    """
-    dimTags = [()]
-    for particle in particles:
-        dimTags.append(
-            (
-                3,
-                gmsh.model.occ.addSphere(
-                    particle[0], particle[1], particle[2], particle[6]
-                ),
-            ),
-        )
-    # Erasing empty tuples from list
-    return list(filter(lambda x: x != (), dimTags))
-
-
-def create_bounding_box(
-    x: float = 0,
-    y: float = 0,
-    z: float = 0,
-    dx: float = 100,
-    dy: float = 100,
-    dz: float = 100,
-) -> [()]:
-    """
-    Create a bounding box.
-
-    Args:
-    - x (float): X-coordinate of the bounding box (default: 0).
-    - y (float): Y-coordinate of the bounding box (default: 0).
-    - z (float): Z-coordinate of the bounding box (default: 0).
-    - dx (float): X-length of the bounding box (default: 100).
-    - dy (float): Y-length of the bounding box (default: 100).
-    - dz (float): Z-length of the bounding box (default: 100).
-
-    Returns:
-    - list of tuples: List containing dimension and tag of the bounding box.
-    """
-    return [(3, gmsh.model.occ.addBox(x, y, z, dx, dy, dz))]
+from os.path import exists
+from os import truncate
 
 
 def update_positions(particles: [()], time_step: float) -> None:
@@ -141,9 +52,9 @@ def simulate_movement_of_particles(
         time += time_step
 
 
-def get_mesh_params(msh_filename: str) -> [()]:
+def get_mesh_params(msh_filename: str, out_filename: str = "mesh.txt") -> [()]:
     """
-    Parse mesh data from a Gmsh .msh file and write information to 'mesh.hdf5'.
+    Parse mesh data from a Gmsh .msh file and write information to 'mesh.txt'.
 
     Args:
     - msh_filename (str): Filename of the Gmsh .msh file to parse.
@@ -178,6 +89,9 @@ def get_mesh_params(msh_filename: str) -> [()]:
         elTags, nodeTags = gmsh.model.mesh.getElementsByType(2, -1)
         nodeTags = nodeTags.reshape((-1, 3))
 
+        if exists(out_filename):
+            truncate(out_filename, 0)
+
         total_surface_area = 0.0
         triangles = []
         for triangle_id, nodes in zip(elTags, nodeTags):
@@ -190,12 +104,12 @@ def get_mesh_params(msh_filename: str) -> [()]:
             a = numpy.sqrt(numpy.dot(xyz1 - xyz2, xyz1 - xyz2))
             b = numpy.sqrt(numpy.dot(xyz1 - xyz3, xyz1 - xyz3))
             c = numpy.sqrt(numpy.dot(xyz2 - xyz3, xyz2 - xyz3))
-
             s = (a + b + c) / 2
+
             # Heron's formula for triangle's area
             dS = numpy.sqrt(s * (s - a) * (s - b) * (s - c))
 
-            with open("mesh.hdf5", "a") as out:
+            with open(out_filename, "a") as out:
                 out.write(
                     f"{triangle_id} \
                     {xyz1[0]} {xyz1[1]} {xyz1[2]} \
@@ -256,32 +170,6 @@ def is_point_belongs_to_the_triangle(point: list, triangleVertices: [()]) -> boo
 
     return not (has_neg and has_pos)
 
-    # rA = numpy.array(triangleVertices[0:3])
-    # rB = numpy.array(triangleVertices[3:6])
-    # rC = numpy.array(triangleVertices[6:9])
-
-    # u = rB - rA
-    # v = rC - rA
-    # w = numpy.cross(u, v)
-
-    # x = numpy.array(point) - rA
-    # y = numpy.array(point) - rB
-
-    # w1 = numpy.cross(x, u)
-    # w2 = numpy.cross(v, y)
-
-    # signs_w1 = numpy.sign(w1)
-    # signs_w2 = numpy.sign(w1)
-
-    # if (
-    #     signs_w1[0] == signs_w2[0]
-    #     and signs_w1[1] == signs_w2[1]
-    #     and signs_w1[2] == signs_w2[2]
-    # ):
-    #     return True
-    # else:
-    #     return False
-
 
 def has_settled(particles: [()], triangles: [()]):
     """
@@ -307,7 +195,7 @@ def has_settled(particles: [()], triangles: [()]):
 def __main__():
     gmsh.initialize(argv)
 
-    particles = read_particle_data("particles.txt")
+    particles = read_particle_data("../results/particles.txt")
     create_bounding_box()
     gmsh.model.occ.synchronize()  # Synchronizing changes
     gmsh.model.mesh.generate(2)  # Generating 2D mesh only for bounded volume
@@ -315,9 +203,9 @@ def __main__():
     create_particles(particles)
     gmsh.model.occ.synchronize()
 
-    gmsh.write("mesh.msh")  # Filling .msh file with points
+    gmsh.write("../results/mesh.msh")  # Filling .msh file with points
 
-    triangles = get_mesh_params("mesh.msh")
+    triangles = get_mesh_params("../results/mesh.msh", "../results/mesh.txt")
     simulate_movement_of_particles(particles, triangles, 0.1, 1)
 
     if "-nopopup" not in argv:
