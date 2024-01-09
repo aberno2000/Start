cf30f941ca37442d0b994b15657245fd4d93afae
Vladislav <vladislav_semykin01@mail.ru>
2024-01-09 12:23:09 +0300

    Simple example, that generates specified count of particles and write theirs data to .txt file. TODO: Replace to .hgf5 format file to improve performance of application

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
+#include <format>
+#include <fstream>
 #include <iostream>
+#include <span>
+#include <string_view>
+
+#include "../include/Particle.hpp"
+#include "../include/RealNumberGenerator.hpp"
+#include "../include/Settings.hpp"
+
+void writeInFile(std::span<ParticleGeneric const> particles, std::string_view filename)
+{
+    std::ofstream ofs(std::string(filename).c_str());
+    if (!ofs.is_open())
+        return;
+
+    for (auto const &particle : particles)
+        ofs << std::format("{} {} {} {} {} {} {}\n",
+                           particle.getX(), particle.getY(), particle.getZ(),
+                           particle.getVx(), particle.getVy(), particle.getVz(),
+                           particle.getRadius());
+    ofs.close();
+}
+
+ParticlesGeneric createParticles(size_t count)
+{
+    RealNumberGenerator rng;
+    ParticlesGeneric particles(count);
+
+    for (size_t i{}; i < count; ++i)
+        particles[i] = ParticleGeneric(rng.get_double(0, 100),
+                                       rng.get_double(0, 100),
+                                       rng.get_double(0, 100),
+                                       rng.get_double(0, 5),
+                                       rng.get_double(0, 5),
+                                       rng.get_double(0, 5),
+                                       rng.get_double(1, 2.5));
+    return particles;
+}
 
 int main()
 {
+    ParticlesGeneric pg(createParticles(50));
+    writeInFile(std::span<ParticleGeneric const>(pg.data(), pg.size()), "results/particles.txt");
+
     return EXIT_SUCCESS;
 }
