8bc859b6bd510a26d67b087ce9cfb5b71a3ec40d
Vladislav <vladislav_semykin01@mail.ru>
2024-01-10 15:01:24 +0300

    Tests for class VolumeCreator written

--- tests/VolumeCreatorTests.cpp
+++ tests/VolumeCreatorTests.cpp
@@ -1,10 +1,11 @@
+#include <cassert>
 #include <format>
 #include <fstream>
 #include <gmsh.h>
 #include <iostream>
-#include <sstream>
 #include <string_view>
 
+#include "../include/RealNumberGenerator.hpp"
 #include "../include/VolumeCreator.hpp"
 
 SphereVector read_particle_data(std::string_view filename)
@@ -13,56 +14,79 @@ SphereVector read_particle_data(std::string_view filename)
     if (ifs.bad())
         throw std::runtime_error(std::format("Can't open file {}", filename));
 
-    // SphereVector particles;
-    // double x{}, y{}, z{}, Vx{}, Vy{}, Vz{}, radius{};
-    // while (ifs >> x >> y >> z >> Vx >> Vy >> Vz >> radius)
-    // {
-    //     std::cout << x << '\t' << y << '\t' << z << '\n';
-    //     particles.emplace_back(std::make_tuple(x, y, z, radius));
-    // }
-
-    std::ostringstream oss;
-    oss << ifs.rdbuf();
-
-    if (oss.str().empty())
-        throw std::runtime_error(std::format("File {} is empty", filename));
-
-    std::cout << oss.str();
+    SphereVector particles;
+    double x{}, y{}, z{}, Vx{}, Vy{}, Vz{}, radius{};
+    while (ifs >> x >> y >> z >> Vx >> Vy >> Vz >> radius)
+        particles.emplace_back(std::make_tuple(x, y, z, radius));
 
     if (!ifs.eof() && ifs.fail())
         throw std::runtime_error(std::format("Error reading data from file {}", filename));
     ifs.close();
 
-    return {};
+    return particles;
+}
+
+inline void testBoxCreating() { assert(VolumeCreator::createBox(10, 10, 10, 10, 20, 30) == 1); }
+inline void testSphereCreating() { assert(VolumeCreator::createSphere(5, 5, 5, 5) == 2); }
+inline void testCylinderCreating() { assert(VolumeCreator::createCylinder(0, 0, 0, 10, 20, 30, 5) == 3); }
+inline void testConeCreating() { assert(VolumeCreator::createCone(25, 25, 25, 10, 20, 30, 5, 10) == 4); }
+
+void testSpheresCreating()
+{
+    SphereVector svec{read_particle_data("results/particles.txt")};
+    std::vector<int> sphereTags{VolumeCreator::createSpheres(SphereSpan(svec.begin(), svec.size()))};
+    int tag{5};
+    for (int tag_to_check : sphereTags)
+        assert(tag_to_check == tag++);
+}
+
+SphereVector generateRandomSpheres(int count)
+{
+    SphereVector spheres;
+    RealNumberGenerator rng;
+
+    for (int i{}; i < count; ++i)
+        spheres.emplace_back(std::make_tuple(rng.get_double(-100.0, 100.0),
+                                             rng.get_double(-100.0, 100.0),
+                                             rng.get_double(-100.0, 100.0),
+                                             rng.get_double(0, 100.0)));
+
+    return spheres;
+}
+
+void testsRandom(int test_count)
+{
+    for (int i{}; i < test_count; ++i)
+    {
+        SphereVector spheres{generateRandomSpheres(10)};
+        std::vector<int> tags{VolumeCreator::createSpheres(SphereSpan(spheres.data(), spheres.size()))};
+
+        for (int tag : tags)
+            assert(tag >= 0);
+    }
 }
 
 int main()
 {
     gmsh::initialize();
 
-    int boxTag = VolumeCreator::createBox(10, 10, 10, 10, 20, 30);
-    std::cout << "Created Box with tag: " << boxTag << std::endl;
+    testBoxCreating();
+    testSphereCreating();
+    testCylinderCreating();
+    testConeCreating();
+    testSpheresCreating();
 
-    int sphereTag = VolumeCreator::createSphere(5, 5, 5, 5);
-    std::cout << "Created Sphere with tag: " << sphereTag << std::endl;
+    std::cout << "1 stage: \033[32;1mAll static tests passed successfully!\033[0m\n";
 
-    int cylinderTag = VolumeCreator::createCylinder(0, 0, 0, 10, 20, 30, 5);
-    std::cout << "Created Cylinder with tag: " << cylinderTag << std::endl;
-
-    int coneTag = VolumeCreator::createCone(25, 25, 25, 10, 20, 30, 5, 10);
-    std::cout << "Created Cone with tag: " << coneTag << std::endl;
-
-    SphereVector svec{read_particle_data("../results/particles.txt")};
-    std::vector<int> sphereTags{VolumeCreator::createSpheres(SphereSpan(svec.begin(), svec.size()))};
-    for (int tag : sphereTags)
-        std::cout << tag << '\t';
-    std::endl(std::cout);
+    int test_count{1'000'000};
+    testsRandom(test_count);
+    std::cout << std::format("2 stage: \033[32;1mAll {} random tests passed successfully!\033[0m\n",
+                             test_count);
 
     gmsh::model::occ::synchronize();
     gmsh::model::mesh::generate(2);
 
     gmsh::fltk::run();
-
     gmsh::finalize();
 
     return EXIT_SUCCESS;
