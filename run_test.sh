005771c0bd61d091821fd08ccc715f5d589a452d
Vladislav <vladislav_semykin01@mail.ru>
2024-01-10 13:28:34 +0300

    Script adapted to GMSH library, compilation changed -> depends on including lgmsh

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,12 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-g++ $1 -std=c++20 -Wall -Wpedantic -Wextra -Werror
+if grep -q "#include <gmsh.h>" "$1"; then
+    g++ $1 src/VolumeCreator.cpp -std=c++20 -lgmsh -Wall -Wpedantic -Wextra -Werror
+else
+    g++ $1 -std=c++20 -Wall -Wpedantic -Wextra -Werror
+fi
+
 if [ $? -eq 0 ]; then
     echo -e "\e[32;1mFile '$1' successfully compiled\e[0m"
 else
