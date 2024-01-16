654eb4af5e82d8305495d8fca1085d948551da44
Vladislav <vladislav_semykin01@mail.ru>
2024-01-16 09:48:09 +0300

    Compiling build enhanced: 1) Added usage hint if smth goes wrong, 2) Added flag to rebuild the project from the scratch, 3) Added opportunity to compile on maximum threads

--- compile.sh
+++ compile.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-# Rebuild from null each time
-# rm -rf build
-mkdir -p build && cd build
-cmake .. && make
+usage() {
+    echo "Usage: $0 [-r|--rebuild]"
+    echo "  -r, --rebuild   Clean and rebuild the project from scratch."
+    exit 1
+}
+
+# Default behavior does not clean build
+REBUILD=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -r | --rebuild)
+        REBUILD=true
+        shift # Remove argument name from processing
+        ;;
+    *) # Unknown option
+        usage
+        ;;
+    esac
+done
+
+# Clean build directory if requested
+if [ "$REBUILD" = true ]; then
+    echo "Rebuilding from the scratch..."
+    rm -rfv build
+fi
+
+mkdir -pv build && cd build
+cmake .. && make -j$(nproc)
