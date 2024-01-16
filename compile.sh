67e1e62af8939fbc90bed32e65b27549048d6296
Vladislav <vladislav_semykin01@mail.ru>
2024-01-16 09:51:01 +0300

    Compiling script improved: Added opportunity to explicitly specify number of threads for building

--- compile.sh
+++ compile.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [-r|--rebuild]"
-    echo "  -r, --rebuild   Clean and rebuild the project from scratch."
+    echo "Usage: $0 [-r|--rebuild] [-j <num_ths>]"
+    echo "  -r, --rebuild       Clean and rebuild the project from scratch."
+    echo "  -j <num_ths>        Specify the number of threads to run simultaneously."
+    echo "                      If not specified, use all available cores."
     exit 1
 }
 
-# Default behavior does not clean build
-REBUILD=false
+REBUILD=false     # Default behavior does not clean build
+NUM_JOBS=$(nproc) # Default to all available processors
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
     -r | --rebuild)
         REBUILD=true
-        shift # Remove argument name from processing
+        ;;
+    -j)
+        if [[ -n $2 && $2 =~ ^[0-9]+$ ]]; then
+            NUM_JOBS=$2
+            shift # Remove argument name from processing
+        else
+            echo "Error: -j requires a numeric argument."
+            usage
+        fi
         ;;
     *) # Unknown option
         usage
         ;;
     esac
+    shift # Move to next argument or value
 done
 
 # Clean build directory if requested
