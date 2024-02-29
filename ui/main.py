e5f2ff3dbce8e4928c955e36603e7fe1c1dcd49d
Vladislav <vladislav_semykin01@mail.ru>
2024-02-29 16:32:33 +0300

    Default app style edited

--- ui/main.py
+++ ui/main.py
@@ -7,17 +7,9 @@ check_and_install_packages(["numpy", "h5py", "gmsh", "matplotlib", "PyQt5", "vtk
 from PyQt5.QtWidgets import QApplication
 from window import WindowApp
 
-dark_stylesheet = """
-QWidget {
-    color: #b1b1b1;
-    background-color: #313131;
-}
-"""
-
 
 def main():    
     app = QApplication(sys.argv)
-    app.setStyleSheet(dark_stylesheet)
     main_window = WindowApp()
     main_window.show()
     sys.exit(app.exec_())
