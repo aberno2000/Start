2a3863fc17d5778dc7531ca414a413dc1fdbde00
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-03-05 17:19:53 +0300

    Script for creating executable for Linux

--- create_executable.sh
+++ create_executable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./compile.sh
-pyinstaller --onefile --add-binary='./argos_nia_start:.' --paths=./ui ui/main.py
+pyinstaller --onefile --add-binary='./argos_nia_start:.' --paths=./ui --name NIA_Start ui/main.py
+pyinstaller NIA_Start.spec
 
