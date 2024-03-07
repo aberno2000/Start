403b7f7d92ebf89ad7d9ec957090b07fc07d3d25
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-03-07 13:45:11 +0300

    Added simple customization of the scalarbar

--- ui/tabs/results_tab.py
+++ ui/tabs/results_tab.py
@@ -1,25 +1,34 @@
-import vtk, json
-from PyQt5.QtGui import QKeyEvent
-from PyQt5.QtWidgets import QVBoxLayout, QWidget
-from PyQt5.QtCore import Qt
+from vtk import ( 
+    vtkAxesActor, vtkOrientationMarkerWidget, vtkRenderer
+)
+from PyQt5.QtWidgets import (
+    QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QSpacerItem, 
+    QSizePolicy, QMenu, QAction, QFontDialog, QDialog, QLabel, 
+    QLineEdit, QMessageBox, QColorDialog
+)
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QIcon
 from util.mesh_renderer import MeshRenderer
 from data.hdf5handler import HDF5Handler
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
-from util.util import align_view_by_axis, save_scene, load_scene
+from util.util import align_view_by_axis
+from util.styles import DEFAULT_QLINEEDIT_STYLE
 
 
 class ResultsTab(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.layout = QVBoxLayout(self)
+        self.toolbarLayout = QHBoxLayout(self)
+        
         self.setup_ui()
         self.setup_axes()
-
+        
 
     def setup_axes(self):
-        self.axes_actor = vtk.vtkAxesActor()
-        self.axes_widget = vtk.vtkOrientationMarkerWidget()
+        self.axes_actor = vtkAxesActor()
+        self.axes_widget = vtkOrientationMarkerWidget()
         self.axes_widget.SetOrientationMarker(self.axes_actor)
         self.axes_widget.SetInteractor(self.vtkWidget.GetRenderWindow().GetInteractor())
         self.axes_widget.SetViewport(0.0, 0.0, 0.2, 0.2)
@@ -28,29 +37,33 @@ class ResultsTab(QWidget):
         
 
     def setup_ui(self):
+        self.setup_toolbar()
+        
         self.vtkWidget = QVTKRenderWindowInteractor(self)
-        self.layout.addWidget(self.vtkWidget)
-
-        self.renderer = vtk.vtkRenderer()
-        self.renderer.SetLayer(0)
+        self.renderer = vtkRenderer()
         self.vtkWidget.GetRenderWindow().AddRenderer(self.renderer)
 
         self.interactor = self.vtkWidget.GetRenderWindow().GetInteractor()
         self.interactorStyle = vtkInteractorStyleTrackballCamera()
         self.interactor.SetInteractorStyle(self.interactorStyle)
         self.interactor.Initialize()
-
+        
+        self.toolbarLayout.addWidget(self.scalarBarSettingsButton)
+        self.layout.addLayout(self.toolbarLayout)
+        self.layout.addWidget(self.vtkWidget)
+        self.setLayout(self.layout)
+        
     
-    def add_colorbar(self, title='Color Scale'):
-        scalarBar = vtk.vtkScalarBarActor()
-        scalarBar.SetTitle(title)
-        scalarBar.SetNumberOfLabels(4)
+    def setup_toolbar(self):       
+        self.scalarBarSettingsButton = QPushButton()
+        self.scalarBarSettingsButton.clicked.connect(self.show_context_menu)
+        self.scalarBarSettingsButton.setIcon(QIcon("icons/settings.png"))
+        self.scalarBarSettingsButton.setIconSize(QSize(32, 32))
+        self.scalarBarSettingsButton.setFixedSize(QSize(32, 32))
+        self.scalarBarSettingsButton.setToolTip('Scalar bar settings')
         
-        # Assuming you have a lookup table set up for your mesh actor
-        scalarBar.SetLookupTable(self.actor.GetMapper().GetLookupTable())
-        
-        self.renderer.AddActor2D(scalarBar)
-        self.vtkWidget.GetRenderWindow().Render()
+        self.spacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.toolbarLayout.addSpacerItem(self.spacer)
 
 
     def update_plot(self, hdf5_filename):
@@ -63,11 +76,11 @@ class ResultsTab(QWidget):
         self.mesh_renderer = MeshRenderer(self.mesh)
         self.mesh_renderer.renderer = self.renderer
         self.mesh_renderer.render_mesh()
-        self.mesh_renderer.add_scalar_bar()
+        self.mesh_renderer.add_colorbar('Particle Count')
         
         self.renderer.ResetCamera()
         self.vtkWidget.GetRenderWindow().Render()
-       
+        
         
     def clear_plot(self):
         self.renderer.RemoveAllViewProps()
@@ -77,3 +90,114 @@ class ResultsTab(QWidget):
     def align_view_by_axis(self, axis: str):
         align_view_by_axis(axis, self.renderer, self.vtkWidget)
 
+
+    def show_context_menu(self):
+        context_menu = QMenu(self)
+
+        # Add actions for changing scale, font, and division number
+        action_change_scale = QAction('Change Scale', self)
+        action_change_font = QAction('Change Font', self)
+        action_change_divs = QAction('Change Number of Divisions', self)
+
+        action_change_scale.triggered.connect(self.change_scale)
+        action_change_font.triggered.connect(self.change_font)
+        action_change_divs.triggered.connect(self.change_division_number)
+
+        context_menu.addAction(action_change_scale)
+        context_menu.addAction(action_change_font)
+        context_menu.addAction(action_change_divs)
+
+        context_menu.exec_(self.mapToGlobal(self.scalarBarSettingsButton.pos()))
+
+    
+    def change_scale(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle('Change Scalar Bar Scale')
+        layout = QVBoxLayout(dialog)
+
+        # Width input
+        width_label = QLabel('Width (as fraction of window width, 0-1):', dialog)
+        layout.addWidget(width_label)
+        width_input = QLineEdit(dialog)
+        width_input.setStyleSheet(DEFAULT_QLINEEDIT_STYLE)
+        layout.addWidget(width_input)
+
+        # Height input
+        height_label = QLabel('Height (as fraction of window height, 0-1):', dialog)
+        layout.addWidget(height_label)
+        height_input = QLineEdit(dialog)
+        height_input.setStyleSheet(DEFAULT_QLINEEDIT_STYLE)
+        layout.addWidget(height_input)
+
+        apply_button = QPushButton('Apply', dialog)
+        layout.addWidget(apply_button)
+        apply_button.clicked.connect(lambda: self.apply_scale(width_input.text(), height_input.text()))
+
+        dialog.setLayout(layout)
+        dialog.exec_()
+
+    def apply_scale(self, width_str, height_str):
+        try:
+            width = float(width_str)
+            height = float(height_str)
+            if 0 <= width <= 1 and 0 <= height <= 1:
+                self.mesh_renderer.scalarBar.SetWidth(width)
+                self.mesh_renderer.scalarBar.SetHeight(height)
+                self.vtkWidget.GetRenderWindow().Render()
+            else:
+                QMessageBox.warning(self, "Invalid Scale", "Width and height must be between 0 and 1")
+        except ValueError:
+            QMessageBox.warning(self, "Invalid Input", "Width and height must be numeric")
+
+    def change_font(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            color = QColorDialog.getColor()
+            if color.isValid():
+                # Convert QColor to a normalized RGB tuple that VTK expects (range 0 to 1)
+                color_rgb = (color.red() / 255, color.green() / 255, color.blue() / 255)
+                
+                text_property = self.mesh_renderer.scalarBar.GetLabelTextProperty()
+                text_property.SetFontFamilyAsString(font.family())
+                text_property.SetFontSize(font.pointSize())
+                text_property.SetBold(font.bold())
+                text_property.SetItalic(font.italic())
+                text_property.SetColor(color_rgb)
+
+                # Apply changes to title text property if needed
+                title_text_property = self.mesh_renderer.scalarBar.GetTitleTextProperty()
+                title_text_property.SetFontFamilyAsString(font.family())
+                title_text_property.SetFontSize(font.pointSize())
+                title_text_property.SetBold(font.bold())
+                title_text_property.SetItalic(font.italic())
+                title_text_property.SetColor(color_rgb)
+
+                self.vtkWidget.GetRenderWindow().Render()
+                
+    def change_division_number(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle('Change Division Number')
+        layout = QVBoxLayout(dialog)
+
+        # Width input
+        divs_label = QLabel('Count of divisions:', dialog)
+        layout.addWidget(divs_label)
+        divs_input = QLineEdit(dialog)
+        divs_input.setStyleSheet(DEFAULT_QLINEEDIT_STYLE)
+        layout.addWidget(divs_input)
+
+        apply_button = QPushButton('Apply', dialog)
+        layout.addWidget(apply_button)
+        apply_button.clicked.connect(lambda: self.apply_divs(divs_input.text()))
+
+        dialog.setLayout(layout)
+        dialog.exec_()
+    
+    def apply_divs(self, divs_str):
+        try:
+            divs = int(divs_str)
+            self.mesh_renderer.scalarBar.SetNumberOfLabels(divs)
+            self.mesh_renderer.set_annotations(divs)
+            self.vtkWidget.GetRenderWindow().Render()
+        except ValueError:
+            QMessageBox.warning(self, "Invalid Input", "Division number must be numeric")
--- ui/util/mesh_renderer.py
+++ ui/util/mesh_renderer.py
@@ -1,38 +1,86 @@
-import vtk
+from vtk import(
+    vtkRenderer, vtkRenderWindow, vtkRenderWindowInteractor,
+    vtkLookupTable, vtkPoints, vtkPolyData, vtkCellArray, vtkTriangle,
+    vtkFloatArray, vtkPolyDataMapper, vtkActor, vtkScalarBarActor,
+    vtkStringArray
+)
+from numpy import round, linspace
 
 class MeshRenderer:
     def __init__(self, mesh):
         self.mesh = mesh
-        self.renderer = vtk.vtkRenderer()
-        self.renderWindow = vtk.vtkRenderWindow()
+        self.renderer = vtkRenderer()
+        self.renderWindow = vtkRenderWindow()
         self.renderWindow.AddRenderer(self.renderer)
-        self.interactor = vtk.vtkRenderWindowInteractor()
+        
+        self.interactor = vtkRenderWindowInteractor()
         self.interactor.SetRenderWindow(self.renderWindow)
+        
+        self.scalarBar = vtkScalarBarActor()
+        self.default_num_labels = 5 # Default labels count
         self.setup_colormap()
-
+        
+        # Default style of the scalar bar
+        self.scalarBar.SetWidth(0.1)
+        self.scalarBar.SetHeight(0.75)
+        text_property = self.scalarBar.GetLabelTextProperty()
+        text_property.SetFontSize(12)
+        text_property.SetFontFamilyAsString("Noto Sans SemiBold")
+        text_property.SetBold(True)
+        text_property.SetItalic(False)
+        text_property.SetColor(0, 0, 0)
+        
+        title_text_property = self.scalarBar.GetTitleTextProperty()
+        title_text_property.SetFontSize(12)
+        title_text_property.SetFontFamilyAsString("Noto Sans SemiBold")
+        title_text_property.SetBold(True)
+        title_text_property.SetItalic(False)
+        title_text_property.SetColor(0, 0, 0)
+    
 
     def setup_colormap(self):
         self.max_count = max(triangle[5] for triangle in self.mesh)
-        self.lookupTable = vtk.vtkLookupTable()
+        self.lookupTable = vtkLookupTable()
         self.lookupTable.SetNumberOfTableValues(256)
         self.lookupTable.SetRange(0, self.max_count)
         for i in range(256):
             ratio = i / 255.0
             self.lookupTable.SetTableValue(i, ratio, 0, 1 - ratio)
         self.lookupTable.Build()
+        self.set_annotations(self.default_num_labels)
+        
+        
+    def set_annotations(self, num_labels=5):
+        valuesArray = vtkFloatArray()
+        annotationsArray = vtkStringArray()
+        unique_values = sorted(set(triangle[5] for triangle in self.mesh))
+        
+        # Function to pick N evenly spaced elements including the first and the last
+        def pick_n_uniformly(lst, n):
+            if n >= len(lst):
+                return lst
+            indeces = round(linspace(0, len(lst) - 1, n)).astype(int)
+            return [lst[index] for index in indeces]
+    
+        uniform_values = pick_n_uniformly(unique_values, num_labels)
+        for value in uniform_values:
+            valuesArray.InsertNextValue(value)
+            annotationsArray.InsertNextValue(f"{value}")
+        
+        self.lookupTable.SetAnnotations(valuesArray, annotationsArray)
 
 
     def render_mesh(self):
         for meshTriangle in self.mesh:
-            points = vtk.vtkPoints()
-            polyData = vtk.vtkPolyData()
-            cells = vtk.vtkCellArray()
+            points = vtkPoints()
+            polyData = vtkPolyData()
+            cells = vtkCellArray()
 
             for vertex in meshTriangle[1:4]:
                 points.InsertNextPoint(vertex)
             
             # Create a polygon for the points
-            triangle = vtk.vtkTriangle()
+            triangle = vtkTriangle()
             triangle.GetPointIds().SetId(0, 0)
             triangle.GetPointIds().SetId(1, 1)
             triangle.GetPointIds().SetId(2, 2)
@@ -42,29 +90,31 @@ class MeshRenderer:
             polyData.SetPolys(cells)
 
             # Map the count value (scalar) to a color
-            scalars = vtk.vtkFloatArray()
+            scalars = vtkFloatArray()
             scalars.SetNumberOfComponents(1)
             scalars.InsertNextValue(meshTriangle[5])
 
             polyData.GetCellData().SetScalars(scalars)
 
-            mapper = vtk.vtkPolyDataMapper()
+            mapper = vtkPolyDataMapper()
             mapper.SetInputData(polyData)
             mapper.SetScalarRange(0, self.max_count)
             mapper.SetLookupTable(self.lookupTable)
 
-            actor = vtk.vtkActor()
-            actor.SetMapper(mapper)
-            self.renderer.AddActor(actor)
+            self.actor = vtkActor()
+            self.actor.SetMapper(mapper)
+            self.renderer.AddActor(self.actor)
 
+        
+    def add_colorbar(self, title='Color Scale', range=None): 
+        self.scalarBar.SetTitle(title)
+        self.scalarBar.SetNumberOfLabels(self.default_num_labels)
 
+        if range:
+            self.actor.GetMapper().GetLookupTable().SetRange(range[0], range[1])
 
-    def add_scalar_bar(self):
-        scalarBar = vtk.vtkScalarBarActor()
-        scalarBar.SetLookupTable(self.lookupTable)
-        scalarBar.SetTitle("Particle Count")
-        scalarBar.SetNumberOfLabels(4)
-        self.renderer.AddActor2D(scalarBar)
+        self.scalarBar.SetLookupTable(self.actor.GetMapper().GetLookupTable())
+        self.renderer.AddActor2D(self.scalarBar)
 
 
     def show(self):
@@ -72,3 +122,4 @@ class MeshRenderer:
         self.add_scalar_bar()
         self.renderWindow.Render()
         self.interactor.Start()
+        
