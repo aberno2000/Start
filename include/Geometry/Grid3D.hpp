1093ba625f40d29eb4153519b1fbcdfbd3e10d86
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-03-18 11:45:57 +0300

    Implementation of 3D grid with uniform cubic grid

---
+++ include/Geometry/Grid3D.hpp
@@ -0,0 +1,80 @@
+#ifndef GRID3D_HPP
+#define GRID3D_HPP
+
+#include <CGAL/Bbox_3.h>
+#include <map>
+
+#include "Mesh.hpp"
+
+#define MAX_GRID_SIZE 0x7A1200 ///< Maximum allowed grid size to prevent memory overflow (default: 8'000'000).
+
+/// @brief Represents a 3D grid index with x, y, and z components.
+struct GridIndex
+{
+    short m_x{}, m_y{}, m_z{};
+
+    GridIndex() {}
+    GridIndex(short x, short y, short z) : m_x(x), m_y(y), m_z(z) {}
+    bool operator==(GridIndex const &other) const { return m_x == other.m_x && m_y == other.m_y && m_z == other.m_z; }
+    bool operator<(GridIndex const &other) const { return std::tie(m_x, m_y, m_z) < std::tie(other.m_x, other.m_y, other.m_z); }
+};
+
+/// @brief Represents a 3D grid that maps tetrahedrons to their containing cells.
+class Grid3D
+{
+private:
+    std::map<size_t, std::vector<GridIndex>> m_tetrahedronCells; ///< Each tetrahedron contains list of cells. Key - ID of tetrahedron, Value - list of cells.
+    double m_cubeEdgeSize{};                                     ///< Edge size of the cell.
+    short m_divisionsX{}, m_divisionsY{}, m_divisionsZ{};        ///< Count of divisions by each axis.
+    CGAL::Bbox_3 m_commonBbox;                                   ///< Common boundary box.
+    MeshTetrahedronParamVector m_meshParams;                     ///< Saving mesh params from ctor.
+
+public:
+    /**
+     * @brief Ctor with params.
+     * @param tetrahedrons Tetrahedron mesh params.
+     * @param edgeSize Size of the cube edge.
+     */
+    Grid3D(MeshTetrahedronParamVector const &meshParams, double edgeSize);
+
+    /**
+     * @brief Getter for grid index by spatial position of some object.
+     * @param x Position by X-axis.
+     * @param y Position by Y-axis.
+     * @param z Position by Z-axis.
+     * @return Grid index.
+     */
+    GridIndex getGridIndexByPosition(double x, double y, double z) const;
+
+    /**
+     * @brief Getter for grid index by spatial position of some object.
+     * @param point `CGAL::Point_3` point.
+     * @return Grid index.
+     */
+    GridIndex getGridIndexByPosition(Point3 const &point) const;
+
+    /// @return The total number of cells.
+    constexpr size_t size() const { return static_cast<size_t>(m_divisionsX * m_divisionsY * m_divisionsZ); }
+
+    /// @return The edge size of the cells.
+    constexpr double edgeSize() const { return m_cubeEdgeSize; }
+
+    /// @brief Prints a list of which tetrahedrons intersect with which grid cells.
+    void printGrid() const;
+
+    /**
+     * @brief Retrieves the mesh parameters for a tetrahedron given its ID.
+     * @param tetrahedronId The ID of the tetrahedron.
+     * @return The mesh parameters for the specified tetrahedron.
+     */
+    MeshTetrahedronParam getTetrahedronMeshParamById(size_t tetrahedronId) const;
+
+    /**
+     * @brief Retrieves the list of tetrahedrons that intersect a specific grid cell.
+     * @param index The index of the grid cell.
+     * @return A vector of tetrahedrons that intersect with the specified cell.
+     */
+    MeshTetrahedronParamVector getTetrahedronsByGridIndex(GridIndex const &index) const;
+};
+
+#endif // !GRID3D_HPP
---
+++ src/Grid3D.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <ranges>
+#include <utility>
+#include <vector>
+
+#include "../include/Geometry/Grid3D.hpp"
+
+Grid3D::Grid3D(MeshTetrahedronParamVector const &meshParams, double edgeSize) : m_cubeEdgeSize(edgeSize)
+{
+    if (meshParams.empty())
+        return;
+    m_meshParams = meshParams;
+
+    // 1. Defining one common boundary box by merging all bboxes of tetrahedrons
+    m_commonBbox = std::get<1>(meshParams.front()).bbox();
+    for (auto const &[id, tetr, volume] : meshParams)
+        m_commonBbox += tetr.bbox();
+
+    // 2. Calculating divisions for each axis
+    m_divisionsX = static_cast<short>(std::ceil((m_commonBbox.xmax() - m_commonBbox.xmin()) / m_cubeEdgeSize));
+    m_divisionsY = static_cast<short>(std::ceil((m_commonBbox.ymax() - m_commonBbox.ymin()) / m_cubeEdgeSize));
+    m_divisionsZ = static_cast<short>(std::ceil((m_commonBbox.zmax() - m_commonBbox.zmin()) / m_cubeEdgeSize));
+
+    // 3. Limitation on grid cells
+    if (m_divisionsX * m_divisionsY * m_divisionsZ > MAX_GRID_SIZE)
+        throw std::runtime_error("The grid is too small, you risk to overflow your memory with it");
+
+    // 4. Mapping each tetrahedron with cells
+    for (auto const &[id, tetra, volume] : meshParams)
+    {
+        for (short x = 0; x < m_divisionsX; ++x)
+            for (short y = 0; y < m_divisionsY; ++y)
+                for (short z = 0; z < m_divisionsZ; ++z)
+                {
+                    CGAL::Bbox_3 cellBox(
+                        m_commonBbox.xmin() + x * edgeSize,
+                        m_commonBbox.ymin() + y * edgeSize,
+                        m_commonBbox.zmin() + z * edgeSize,
+                        m_commonBbox.xmin() + (x + 1) * edgeSize,
+                        m_commonBbox.ymin() + (y + 1) * edgeSize,
+                        m_commonBbox.zmin() + (z + 1) * edgeSize);
+
+                    if (CGAL::do_overlap(cellBox, tetra.bbox()))
+                        m_tetrahedronCells[id].emplace_back(x, y, z);
+                }
+    }
+}
+
+GridIndex Grid3D::getGridIndexByPosition(double x, double y, double z) const
+{
+    return {
+        std::clamp(short((x - m_commonBbox.xmin()) / m_cubeEdgeSize), short(0), static_cast<short>(m_divisionsX - 1)),
+        std::clamp(short((y - m_commonBbox.ymin()) / m_cubeEdgeSize), short(0), static_cast<short>(m_divisionsY - 1)),
+        std::clamp(short((z - m_commonBbox.zmin()) / m_cubeEdgeSize), short(0), static_cast<short>(m_divisionsZ - 1))};
+}
+
+GridIndex Grid3D::getGridIndexByPosition(Point3 const &point) const
+{
+    double x{CGAL_TO_DOUBLE(point.x())},
+        y{CGAL_TO_DOUBLE(point.y())},
+        z{CGAL_TO_DOUBLE(point.z())};
+    return {
+        std::clamp(short((x - m_commonBbox.xmin()) / m_cubeEdgeSize), short(0), static_cast<short>(m_divisionsX - 1)),
+        std::clamp(short((y - m_commonBbox.ymin()) / m_cubeEdgeSize), short(0), static_cast<short>(m_divisionsY - 1)),
+        std::clamp(short((z - m_commonBbox.zmin()) / m_cubeEdgeSize), short(0), static_cast<short>(m_divisionsZ - 1))};
+}
+
+void Grid3D::printGrid() const
+{
+    for (auto const &[id, cells] : m_tetrahedronCells)
+    {
+        std::cout << std::format("Tetrahedron[{}] is in cells: ", id);
+        for (auto const &[x, y, z] : cells)
+            std::cout << std::format("[{}][{}][{}] ", x, y, z);
+        std::cout << std::endl;
+    }
+}
+
+MeshTetrahedronParam Grid3D::getTetrahedronMeshParamById(size_t tetrahedronId) const
+{
+    auto it{std::ranges::find_if(m_meshParams,
+                                 [tetrahedronId](MeshTetrahedronParam const &param)
+                                 {
+                                     return std::get<0>(param) == tetrahedronId;
+                                 })};
+
+    if (it != m_meshParams.cend())
+        return *it;
+
+    throw std::runtime_error("Tetrahedron ID not found: " + std::to_string(tetrahedronId));
+}
+
+MeshTetrahedronParamVector Grid3D::getTetrahedronsByGridIndex(GridIndex const &index) const
+{
+    MeshTetrahedronParamVector meshParams;
+    for (auto const &[tetrId, cells] : m_tetrahedronCells)
+        if (std::ranges::find(cells.begin(), cells.end(), index) != cells.end())
+            meshParams.emplace_back(getTetrahedronMeshParamById(tetrId));
+    return meshParams;
+}
