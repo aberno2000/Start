f5ea5330b3dbdc31e6e975470b81902736fade07
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-04-01 10:49:59 +0300

    Driver code simplified using new classes

--- src/tracking_particles_in_volume.cpp
+++ src/tracking_particles_in_volume.cpp
@@ -1,32 +1,13 @@
 #include "../include/Generators/VolumeCreator.hpp"
-#include "../include/Geometry/Grid3D.hpp"
 #include "../include/Particles/Particles.hpp"
 #include "../include/Utilities/MatrixEquationSolver.hpp"
+#include "../include/Utilities/ParticleTracker.hpp"
 
 static constexpr std::string_view k_mesh_filename{"test.msh"};
 static constexpr size_t k_particles_count{100};
 
-/**
- * @brief Checker for point inside the tetrahedron.
- * @param point `Point_3` from CGAL.
- * @param tetrahedron `Tetrahedron_3` from CGAL.
- * @return `true` if point within the tetrahedron, otherwise `false`.
- */
-bool isParticleInsideTetrahedron(Particle const &particle, MeshTetrahedronParam const &meshParam)
-{
-    CGAL::Oriented_side oriented_side{std::get<1>(meshParam).oriented_side(particle.getCentre())};
-    if (oriented_side == CGAL::ON_POSITIVE_SIDE)
-        return true;
-    else if (oriented_side == CGAL::ON_NEGATIVE_SIDE)
-        return false;
-    else
-        // TODO: Correctly handle case when particle is on boundary of tetrahedron.
-        return true;
-}
-
 int main(int argc, char *argv[])
 {
-#pragma region VolumeParticleTracking
     // 1. Creating particles.
     auto particles{createParticlesWithVelocities(k_particles_count, particle_types::Al)};
 
@@ -48,38 +29,12 @@ int main(int argc, char *argv[])
     // 5. Creating grid.
     Grid3D grid(tetrahedronMesh, edgeSize);
 
-    // 6. Start simulating and track particles in each time moment.
-    std::map<size_t, ParticleVector> particleTracker; ///< Key - tetrahedron ID; Value - particles within tetrahedrons.
+    // 6. Tracking particles inside the tetrahedrons.
     double dt{0.1}, simtime{0.5};
-    for (double t{}; t <= simtime; t += dt)
-    {
-        std::cout << std::format("\033[1;34mTime {}\n\033[0m", t);
-        for (auto &pt : particles)
-        {
-            if (t != 0.0)
-                pt.updatePosition(dt);
+    ParticleTracker tracker(particles, grid, dt, simtime);
+    tracker.trackParticles();
 
-            auto meshParams{grid.getTetrahedronsByGridIndex(grid.getGridIndexByPosition(pt.getCentre()))};
-            for (auto const &meshParam : meshParams)
-                if (isParticleInsideTetrahedron(pt, meshParam))
-                    particleTracker[std::get<0>(meshParam)].emplace_back(pt);
-        }
-
-        // 6.1_opt. Printing results.
-        size_t count{};
-        for (auto const &[tetrId, pts] : particleTracker)
-        {
-            count += pts.size();
-            std::cout << std::format("Tetrahedron[{}]: ", tetrId);
-            for (auto const &pt : pts)
-                std::cout << pt.getId() << ' ';
-            std::endl(std::cout);
-        }
-        std::cout << "Count of particles: " << count << '\n';
-        particleTracker.clear();
-    }
-#pragma endregion
-
+    /* Work with matrices. */
     Teuchos::GlobalMPISession mpiSession(std::addressof(argc), std::addressof(argv));
     Kokkos::initialize(argc, argv);
     {
