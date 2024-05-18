96812a2dc350565ad189bade6bff80eb22f84855
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-05-18 15:51:05 +0300

    Added new default styles for the combobox and lineedit (disabled & enabled)

--- ui/util/styles.py
+++ ui/util/styles.py
@@ -6,6 +6,9 @@ APPSTYLE_BRIGHT = "QWidget { background-color: white; color: white; }"
 APPSTYLE_DEFAULT = "QWidget { background-color: #333; color: white; }"
 
 DEFAULT_QLINEEDIT_STYLE = "QLineEdit { background-color: light gray; color: black; }"
+DEFAULT_COMBOBOX_STYLE = ""
+DEFAULT_DISABLED_QLINEEDIT_STYLE = "QLineEdit { background-color: gray; color: gray; }"
+DEFAULT_DISABLED_COMBOBOX_STYLE = "QComboBox { background-color: gray; color: gray; }"
 
 DEFAULT_FONT_COLOR = 'white'
 
