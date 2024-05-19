2b59fdf25412c44e3512a2180f04c4638f52ee05
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-05-19 14:21:11 +0300

    Using  class in main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,19 @@
-#include "../include/Utilities/CollisionTracker.hpp"
-#include "../include/Utilities/ConfigParser.hpp"
-
-void simulateMovement(ConfigParser const &configObj,
-                      double gasConcentration,
-                      std::string_view mshfilename,
-                      std::string_view hdf5filename)
-{
-    MeshTriangleParamVector mesh;
-
-    // Optimization 1: Artificial space for handling GMSH app.
-    // To finilize GMSH as soon as possible.
-    {
-        GMSHVolumeCreator volumeCreator;
-        mesh = volumeCreator.getMeshParams(mshfilename);
-
-        if (mesh.empty())
-        {
-            ERRMSG(util::stringify("Something wrong with filling of the mesh. Mesh file is ", mshfilename, ". Exiting..."));
-            std::exit(EXIT_FAILURE);
-        }
-    }
-    auto pgs(createParticlesWithEnergy(configObj.getParticlesCount(),
-                                       configObj.getProjective(),
-                                       configObj.getEnergy(),
-                                       util::getParticleSourceCoordsAndDirection()));
-
-    CollisionTracker ct(pgs, mesh, configObj, gasConcentration);
-    auto counterMap{ct.trackCollisions(configObj.getNumThreads())};
-
-    auto mapEnd{counterMap.end()};
-    for (auto &triangle : mesh)
-        if (auto it{counterMap.find(std::get<0>(triangle))}; it != mapEnd)
-            std::get<3>(triangle) = it->second;
-
-    HDF5Handler hdf5handler(hdf5filename);
-    hdf5handler.saveMeshToHDF5(mesh);
-    auto updatedMesh{hdf5handler.readMeshFromHDF5()};
-}
+#include "../include/ParticleTracker.hpp"
 
 int main(int argc, char *argv[])
 {
+    // Initializing global MPI session and Kokkos.
+    Teuchos::GlobalMPISession mpiSession(std::addressof(argc), std::addressof(argv));
+    Kokkos::initialize(argc, argv);
+
     if (argc != 2)
     {
         ERRMSG(util::stringify("Usage: ", argv[0], " <config_file>"));
         return EXIT_FAILURE;
     }
+    ParticleTracker particleTracker(argv[1]);
+    particleTracker.startSimulation();
 
-    std::string configFilename(argv[1]);
-    ConfigParser configParser(configFilename);
-
-    std::string mshfilename(configParser.getMeshFilename()),
-        hdf5filename(mshfilename.substr(0, mshfilename.find(".")) + ".hdf5");
-
-    util::checkRestrictions(configParser.getTimeStep(), configParser.getParticlesCount(), configFilename);
-    double gasConcentration(util::calculateConcentration(configFilename));
-
-    if (gasConcentration < gasConcentrationMinimalValue)
-        WARNINGMSG(util::stringify("Something wrong with the concentration of the gas. Its value is ", gasConcentration, ". Simulation might considerably slows down"));
-
-    simulateMovement(configParser, gasConcentration, mshfilename, hdf5filename);
-
+    Kokkos::finalize();
     return EXIT_SUCCESS;
 }
