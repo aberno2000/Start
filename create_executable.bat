4e9903a273e554ce958534f85f2e7693a40db0ca
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-06-13 11:36:59 +0300

    Updated script for the creating executable (Linux/Windows support). Icons and C++ executable involved into the directory 'dist' with Python executable

--- create_executable.bat
+++ create_executable.bat
@@ -1,4 +1,13 @@
 @echo on
-pyinstaller --onefile --add-binary ".\Release\nia_start.exe;." --add-binary ".\Release\gmsh-4.12.dll;." --add-binary ".\Release\libgmp-10.dll;." --add-binary "C:\Users\vladislavsemykin\.conda\envs\startenv\Lib\site-packages\vtk.libs\*.dll;." --paths ".\ui;.\include;.\icons;." --name nia_start ui\main.py
+
+for /f "tokens=2 delims==" %%i in ('"wmic os get localdatetime /value"') do set datetime=%%i
+set current_date=%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4%
+set output_name=nia_start_%current_date%
+
+pyinstaller --onefile --add-binary ".\Release\nia_start.exe;." --add-binary ".\Release\gmsh-4.12.dll;." --add-binary ".\Release\libgmp-10.dll;." --add-data ".\icons;icons" --paths ".\ui;.\include" --name %output_name% ui\main.py
 pyinstaller nia_start.spec
+
+xcopy /E /I .\icons .\dist\icons
+copy .\Release\nia_start.exe .\dist\nia_start.exe
+
 pause
--- create_executable.sh
+++ create_executable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-./compile.sh
-pyinstaller --onefile --add-binary='./nia_start:.' --paths=./ui --name nia_start ui/main.py
+current_date=$(date +"%d.%m.%Y")
+output_name="nia_start_$current_date"
+
+./compile.sh -j 4
+pyinstaller --onefile --add-binary='./nia_start:.' --add-data='./icons:icons' --paths=./ui --name "$output_name" ui/main.py
 pyinstaller nia_start.spec
+
+cp -rv icons/ "dist/icons"
+cp -v nia_start "dist/nia_start"
