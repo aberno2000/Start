763cb8066e3a415fc0366d0751dd671bc04c7fdb
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-06-14 13:24:26 +0300

    In 'Edit' tab added opportunity to change the FPS value for the animation

--- ui/window.py
+++ ui/window.py
@@ -221,6 +221,7 @@ class WindowApp(QMainWindow):
         bg_color_menu.addAction('Custom', lambda: self.change_background_color('custom'))
         
         edit_menu.addAction('Show Shortcuts', self.show_shortcuts)
+        edit_menu.addAction('Change FPS (for animation)', self.results_tab.edit_fps)
 
         # Configurations Menu
         configurations_menu = menu_bar.addMenu('&Configurations')
