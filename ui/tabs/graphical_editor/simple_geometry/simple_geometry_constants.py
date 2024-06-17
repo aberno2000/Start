fc0cc12e263c5d25c8917ccb1ca8c1bf8d428ddf
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-06-17 17:27:50 +0300

    Improved hints for the simple geometry

--- ui/tabs/graphical_editor/simple_geometry/simple_geometry_constants.py
+++ ui/tabs/graphical_editor/simple_geometry/simple_geometry_constants.py
@@ -1,7 +1,13 @@
 DEFAULT_SPHERE_PHI_RESOLUTION = 100
 DEFAULT_SPHERE_THETA_RESOLUTION = 100
-SIMPLE_GEOMETRY_SPHERE_PHI_RESOLUTION_HINT = "Amount of diviations on the revolution of the angle"
-SIMPLE_GEOMETRY_SPHERE_THETA_RESOLUTION_HINT = "Amount of diviations on the revolution of the angle"
+SIMPLE_GEOMETRY_SPHERE_PHI_RESOLUTION_HINT = (
+    "Specifies the number of subdivisions around the circumference of the sphere along the longitudinal (phi) direction. "
+    "Higher values result in a smoother sphere surface.")
+SIMPLE_GEOMETRY_SPHERE_THETA_RESOLUTION_HINT = (
+    "Specifies the number of subdivisions along the latitude (theta) direction of the sphere. "
+    "Higher values result in a smoother sphere surface.")
 
 DEFAULT_CYLINDER_RESOLUTION = 100
-SIMPLE_GEOMETRY_CYLINDER_RESOLUTION_HINT = "Amount of diviations on the revolution of the angle"
+SIMPLE_GEOMETRY_CYLINDER_RESOLUTION_HINT = (
+    "Specifies the number of subdivisions around the circumference of the cylinder. "
+    "Higher values result in a smoother cylindrical surface.")
