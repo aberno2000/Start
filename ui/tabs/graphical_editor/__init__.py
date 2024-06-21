f5af50cb1ef0eaee966795e2ce079f009a98fb16
ViNN280801 <vladislavsemykin@loveit.loveit>
2024-06-21 11:13:38 +0300

    mesh_tree/ dir renamed to mesh_tree_manager/

--- ui/tabs/graphical_editor/__init__.py
+++ ui/tabs/graphical_editor/__init__.py
@@ -1,5 +1,5 @@
 from .interactor import *
-from .mesh_tree import *
+from .mesh_tree_manager import *
 from .simple_geometry import *
 from .particle_source_manager import *
 from .graphical_editor import *
